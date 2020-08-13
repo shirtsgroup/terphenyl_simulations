95047adbc675493fd5cc18a924fae05ce32d7e3f
tlfobe <theodore.fobe@colorado.edu>
2020-08-13 09:31:37 -0600

    started moving independent analysis into their own respectiv jupyter-notebooks

--- analysis/octamer_Rchiral/RHH/remd_sim/h_bonds.py
+++ analysis/octamer_Rchiral/RHH/remd_sim/h_bonds.py
@@ -87,5 +87,88 @@ class HydrogenBondFinder:
             h_bonds.append(frame_h_bond)
         return(n_h_bonds, h_bonds)
             
+def main():
+    import argparse
+    import plotting
+    import matplotlib.pyplot as plt
+    from multiprocessing import Pool
+    import heat_capacity
+    import time
+    import pymbar
 
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument(
+        "--sim_dir_name",
+        required = True,
+        help = "base name of replica directories",
+        type = str
+    )
+    parser.add_argument(
+        "--path",
+        required = True,
+        help = "path to RE exchange simulation directories"
+    )
+    parser.add_argument(
+        "--n_replicas",
+        required = True,
+        help = "number of replicas run in replica exchange simulation",
+        type = int
+    )
+    parser.add_argument(
+        "--sim_id",
+        help = "simulation name ID used in replica exchange simulation",
+        type = str,
+        default = "npt"
+    )
+    parser.add_argument(
+        "--figure_id",
+        required = True,
+        help = "base name for figure generated in this script",
+        type = str
+    )
+    parser.add_argument(
+        "--n_estimated_points",
+        help = "number of points to estimate between maximum and minimum temperature",
+        type = int,
+        default = 200
+    )
+
+    args = parser.parse_args()
+
+    sim_dir_name = args.sim_dir_name
+    path = args.path
+    n_replicas = args.n_replicas
+    sim_id = args.sim_id
+
+    print("Reading Energy Files...")
+    energies, temps = heat_capacity.get_energies(sim_dir_name, path, n_replicas, sim_name = sim_id)
+
+    # Plot energy distributions of RE simualtion
+    plotting.plot_RE_energy_distributions(energies)
+    plt.savefig(args.figure_id + "energy_dist.png")
+
+    # Plot energy trajectory of RE simulation
+    plotting.plot_RE_energy_trajectory(energies)
+    plt.savefig(args.figure_id + "energy_traj.png")
+
+    u_kln, n_samples, t_list, betas = heat_capacity.construct_u_kln_matrix(temps, energies, add_temps = np.linspace(min(temps), max(temps), args.n_estimated_points))
+
+    remd_trajs = REMD_trajectories("/mnt/summit/simulations/octamer_Rchiral/RHH/remd_sim/200K_to_350K", "npt", "whole.xtc","sim", "/mnt/summit/simulations/octamer_Rchiral/RHH/remd_sim/200K_to_350K/sim0/berendsen.gro", np = 1)
+
+    h_bond_finder = h_bonds.HydrsogenBondFinder(remd_trajs.trajs[0][0], remd_trajs.trajs[0][0].top)
+    h_bond_finder.get_donors()
+    h_bond_finder.get_acceptors()
+    n_h_bonds, h_bond_ids = h_bond_finder.get_hydrogen_bonds(remd_trajs.trajs[0])
+
+    pool = Pool(4)
+
+    n_h_bonds_remd, h_bonds_remd = zip(*pool.map(h_bond_finder.get_hydrogen_bonds, remd_trajs.trajs))
+    n_h_bonds_remd = np.array(n_h_bonds_remd)
+
+    h_bonds_kln = np.zeros(len(betas), len(betas), n_h_bonds_remd[1])
+    for k in range(n_h_bonds_remd.shape[0]):
+        for l in range(h_bonds_kln.shape[0]):
+            h_bonds_kln[k,l,:] = n_h_bonds_remd[k]
+
+    mbar_h_bonds = mbar_h_bonds = pymbar.MBAR(u_kln[:,:,:3608], n_samples_h_bonds, verbose = True, relative_tolerance = 1e-10, initial_f_k= None, maximum_iterations=1000)
