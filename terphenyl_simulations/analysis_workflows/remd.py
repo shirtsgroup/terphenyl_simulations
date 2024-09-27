bd5b9a6610f6420788f4c9f43f658dd1c1e4c304
Theodore Fobe <thfo9888@c3cpu-c15-u32-4.rc.int.colorado.edu>
2024-09-27 15:26:30 -0600

    working simulations on alpine, added some generated topologies

--- terphenyl_simulations/analysis_workflows/remd.py
+++ terphenyl_simulations/analysis_workflows/remd.py
@@ -9,6 +9,7 @@ import flow
 import sys
 from flow import FlowProject
 import terphenyl_simulations
+from terphenyl_simulations.utils import replace_all_pattern
 
 # Initialize Signac Project
 
@@ -158,6 +159,8 @@ def apply_hmr_to_topology(job):
     tm = terphenyl_simulations.build.TopologyManager()
     tm.add_topology(output_topology, job.sp["build_foldamer"], "system")
     job.doc["foldamer_topology"] = output_topology
+    for submit_file in glob.glob("submit*.slurm"):
+        replace_all_pattern("TOPOLOGY_FILE", output_topology, submit_file)
 
 
 @FlowProject.pre.after(apply_hmr_to_topology)
@@ -182,7 +185,7 @@ def setup_remd_simulations(job):
 @FlowProject.operation(directives={"fork": True})
 @cd_to_job_dir
 def submit_simulations(job):
-    subprocess.run(["bash", "submit_all.slurm"], shell = True)
+    subprocess.Popen(["bash", "submit_all.slurm"], shell = True)
     subprocess.wait()
     
 
